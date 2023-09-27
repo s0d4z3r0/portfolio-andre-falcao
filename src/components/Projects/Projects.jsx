f99621aff418532bbdc4a67b347dd7bdc79ebbe1
s0d4z3r0 <av.falcao@hotmail.com>
2023-09-27 07:32:10 -0300

    Adicionado projeto Todo ao portifólio

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -4,7 +4,8 @@ import styles from "./Projects.module.css";
 import CardsProjects from "../CardsProjects/CardsProjects";
 
 // Img
-import thumbP1 from "../../assets/thumb.jpg";
+import thumbTodo from "../../assets/thumb-todo.jpg";
+import thumbTotem from "../../assets/thumb-totem.jpg";
 
 // Swiper
 import { Swiper, SwiperSlide } from "swiper/react";
@@ -16,6 +17,19 @@ import "swiper/css/pagination";
 const Projects = () => {
   const projects = [
     {
+      name: "Lista de Tarefas - To-Do (React)",
+      description:
+        "Este projeto foi feito com React + Vite e BootStrap para compor seu estilo e responsividade. Foi feito com base nos conhecimentos aprendidos de React e JS como useState e useEffect, componentização e props, array de objetos, spread operator. Além da responsividade padrão do BootStrap, ainda foi feito alguns ajustes para telas menores com CSS Global e função com evento 'resize' para alterar placeholders de forma dinâmica.",
+      html: true,
+      css: true,
+      js: true,
+      bs: true,
+      react: true,
+      thumb: thumbTodo,
+      projectGit: "https://github.com/s0d4z3r0/todo_react_vite",
+      projectView: "https://s0d4z3r0.github.io/todo_react_vite/",
+    },
+    {
       name: "Sistema de Totem - Hamburgueria",
       description:
         "Este projeto foi realizado com HTML, CSS e JS (JSX - React + Vite), sendo idealizado um projeto de totem para fazer pedidos em estabelecimentos físicos. Conta com uma tela inicial (HOME), seguindo para uma tela de fazer o pedido (MENU). Foi utilizado vários conhecimentos aprendidos do framework React como componentização, router-dom, hooks, css modules, redux, criação e reaproveitamento de states e modais. Foi trabalhado a responsividade seguindo o princípio mobile first, com media querys mínima de 320px.",
@@ -24,7 +38,7 @@ const Projects = () => {
       js: true,
       bs: false,
       react: true,
-      thumb: thumbP1,
+      thumb: thumbTotem,
       projectGit: "https://github.com/s0d4z3r0/totem-react",
       projectView: "https://s0d4z3r0.github.io/totem-react/",
     },
@@ -48,14 +62,14 @@ const Projects = () => {
         <SwiperSlide>
           <br />
           <CardsProjects project={projects[0]} />
+          <br/>
+          <br/>
         </SwiperSlide>
         <SwiperSlide>
           <br />
-          <CardsProjects project={projects[0]} />
-        </SwiperSlide>
-        <SwiperSlide>
-          <br />
-          <CardsProjects project={projects[0]} />
+          <CardsProjects project={projects[1]} />
+          <br/>
+          <br/>
         </SwiperSlide>
       </Swiper>
     </section>
